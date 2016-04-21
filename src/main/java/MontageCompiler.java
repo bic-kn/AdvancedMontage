3e96aff7de4d78fed11c29d29bfd5bcba4c8f5e7
Stefan Helfrich <stefan.helfrich@googlemail.com>
2016-04-21 17:02:20 +0200

    Add ROI and scalebar functionality

--- src/main/java/MontageCompiler.java
+++ src/main/java/MontageCompiler.java
@@ -1,4 +1,5 @@
 import java.awt.Component;
+import java.awt.Point;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -8,6 +9,10 @@ import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.ImageRoi;
 import ij.gui.Overlay;
+import ij.gui.Roi;
+import ij.measure.Calibration;
+import ij.plugin.ScaleBar;
+import ij.plugin.frame.RoiManager;
 import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
 import ij.process.LUT;
@@ -17,6 +22,8 @@ public class MontageCompiler implements Observer {
 	private ImagePlus inputImp;
 	private int maxColumns = -1;
 	private int maxRows = -1;
+	static final String[] locations = {"Upper Right", "Lower Right", "Lower Left", "Upper Left", "At Selection"};
+	static final int UPPER_RIGHT=0, LOWER_RIGHT=1, LOWER_LEFT=2, UPPER_LEFT=3, AT_SELECTION=4;
 	
 	public MontageCompiler(ImagePlus inputImp) {
 		this.inputImp = inputImp;
@@ -26,8 +33,13 @@ public class MontageCompiler implements Observer {
 		Component[] components = panel.getComponents();
 		
 		int columns = numberOfOutputColumns(panel);
-		int rows = numberOfOutputRows(panel);		
-		ImagePlus outputImp = new ImagePlus("Montage", new ByteProcessor(columns*inputImp.getWidth(), rows*inputImp.getHeight()));
+		int rows = numberOfOutputRows(panel);
+		
+		// TODO Integrate padding
+		int outputWidth = computeOutputWidth(columns, 0); //, paddingWidth);
+		int outputHeight = computeOutputHeight(rows, 0); //, paddingWidth);
+		
+		ImagePlus outputImp = new ImagePlus("Montage", new ByteProcessor(outputWidth, outputHeight));
 		
 		for (Component component : components) {
 			if (component instanceof MontageItem) {
@@ -39,7 +51,15 @@ public class MontageCompiler implements Observer {
 		
 		return outputImp;
 	}
+
+	private int computeOutputWidth(int columns, int paddingWidth) {
+		return columns*inputImp.getWidth() + (columns-1)*paddingWidth;
+	}
 	
+	private int computeOutputHeight(int rows, int paddingWidth) {
+		return rows*inputImp.getHeight() + (rows-1)*paddingWidth;
+	}
+
 	void compileItem(MontageItem item, ImagePlus outputImp) {
 		int inputWidth = inputImp.getWidth();
 		int inputHeight = inputImp.getHeight();
@@ -82,14 +102,21 @@ public class MontageCompiler implements Observer {
 		}
 		overlay.add(flattenedImpRoi);
 		
-		// TODO Add scalebar and ROIs
+		// TODO Add scalebar
 		for (MontageItemOverlay blubb : item.getOverlays()) {
 			if (blubb instanceof RoiOverlay) {
-				// TODO Do something
-//				overlay.add(roi);
+				RoiManager roiManager = RoiManager.getInstance();
+				if (roiManager == null) {
+					continue;
+				}
+				
+				Roi[] roisInManager = roiManager.getRoisAsArray();
+				for (Roi roi : roisInManager) {
+					overlay.add(roi);
+				}
 			} else if (blubb instanceof ScalebarOverlay) {
-				// TODO Do something
-//				overlay.add(scalebarRoi);
+				Roi scalebarRoi = generateScalebar(item);
+				overlay.add(scalebarRoi);
 			}
 		}
 		
@@ -148,32 +175,46 @@ public class MontageCompiler implements Observer {
 	/**
 	 * Adapted from {@link ScaleBar}.
 	 */
-//	boolean updateLocation() {
-//		Calibration cal = imp.getCalibration();
-//		barWidthInPixels = (int)(barWidth/cal.pixelWidth);
-//		int width = imp.getWidth();
-//		int height = imp.getHeight();
-//		int fraction = 20;
-//		int x = width - width/fraction - barWidthInPixels;
-//		int y = 0;
-//		if (location.equals(locations[UPPER_RIGHT]))
-//			 y = height/fraction;
-//		else if (location.equals(locations[LOWER_RIGHT]))
-//			y = height - height/fraction - barHeightInPixels - fontSize;
-//		else if (location.equals(locations[UPPER_LEFT])) {
-//			x = width/fraction;
-//			y = height/fraction;
-//		} else if (location.equals(locations[LOWER_LEFT])) {
-//			x = width/fraction;
-//			y = height - height/fraction - barHeightInPixels - fontSize;
-//		} else {
-//			if (roiX==-1)
-//				 return false;
-//			x = roiX;
-//			y = roiY;
-//		}
-//		xloc = x;
-//		yloc = y;
-//		return true;
-//	}
+	private Roi generateScalebar(MontageItem item) {
+		// TODO Get from global settings
+		double barWidth = 4.5d;
+		
+		// TODO Get from global settings
+		String location = "Lower Right";
+		
+		// TODO Get from global settings
+		int barHeightInPixels = 2;
+		
+		// TODO Get from global settings
+		int fontSize = 42;
+		
+		// TODO Get from global settings
+		int paddingWidth = 10;
+		
+		Calibration cal = inputImp.getCalibration();
+		int barWidthInPixels = (int)(barWidth/cal.pixelWidth);
+		
+		int width = inputImp.getWidth();
+		int height = inputImp.getHeight();
+		int fraction = 20;
+		int x = width - width/fraction - barWidthInPixels;
+		int y = 0;
+		
+		if (location.equals(locations[UPPER_RIGHT]))
+			 y = height/fraction;
+		else if (location.equals(locations[LOWER_RIGHT]))
+			y = height - height/fraction - barHeightInPixels - fontSize;
+		else if (location.equals(locations[UPPER_LEFT])) {
+			x = width/fraction;
+			y = height/fraction;
+		} else if (location.equals(locations[LOWER_LEFT])) {
+			x = width/fraction;
+			y = height - height/fraction - barHeightInPixels - fontSize;
+		}
+
+		int xShift = item.getColumn()*inputImp.getWidth() + item.getColumn()*paddingWidth;
+		int yShift = item.getRow()*inputImp.getHeight() + item.getRow()*paddingWidth;
+		
+		return new Roi(xShift + x, yShift + y, barWidthInPixels, barHeightInPixels);
+	}
 }
