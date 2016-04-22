7f9ff9efa3c28bd9a9d43026085bc8e20e024216
Stefan Helfrich <stefan.helfrich@googlemail.com>
2016-04-22 10:21:45 +0200

    Add text for scalebar

--- src/main/java/MontageCompiler.java
+++ src/main/java/MontageCompiler.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.Point;
 import java.util.Observable;
 import java.util.Observer;
@@ -10,6 +12,7 @@ import ij.ImageStack;
 import ij.gui.ImageRoi;
 import ij.gui.Overlay;
 import ij.gui.Roi;
+import ij.gui.TextRoi;
 import ij.measure.Calibration;
 import ij.plugin.ScaleBar;
 import ij.plugin.frame.RoiManager;
@@ -22,6 +25,9 @@ public class MontageCompiler implements Observer {
 	private ImagePlus inputImp;
 	private int maxColumns = -1;
 	private int maxRows = -1;
+	private boolean boldText;
+	private boolean serifFont;
+	private boolean hideText = false;
 	static final String[] locations = {"Upper Right", "Lower Right", "Lower Left", "Upper Left", "At Selection"};
 	static final int UPPER_RIGHT=0, LOWER_RIGHT=1, LOWER_LEFT=2, UPPER_LEFT=3, AT_SELECTION=4;
 	
@@ -102,28 +108,27 @@ public class MontageCompiler implements Observer {
 		}
 		overlay.add(flattenedImpRoi);
 		
-		// TODO Add scalebar
-		for (MontageItemOverlay blubb : item.getOverlays()) {
-			if (blubb instanceof RoiOverlay) {
-				RoiManager roiManager = RoiManager.getInstance();
-				if (roiManager == null) {
-					continue;
-				}
-				
-				Roi[] roisInManager = roiManager.getRoisAsArray();
-				for (Roi roi : roisInManager) {
-					overlay.add(roi);
-				}
-			} else if (blubb instanceof ScalebarOverlay) {
-				Roi scalebarRoi = generateScalebar(item);
-				overlay.add(scalebarRoi);
+		for (MontageItemOverlay itemOverlay : item.getOverlays()) {
+			if (itemOverlay instanceof RoiOverlay) {
+				addROIsToOverlay(overlay);
+			} else if (itemOverlay instanceof ScalebarOverlay) {
+				addScalebarToOverlay(item, overlay);
 			}
 		}
-		
-		
-		
 	}
-	
+
+	private void addROIsToOverlay(Overlay overlay) {
+		RoiManager roiManager = RoiManager.getInstance();
+		if (roiManager == null) {
+			return;
+		}
+
+		Roi[] roisInManager = roiManager.getRoisAsArray();
+		for (Roi roi : roisInManager) {
+			overlay.add(roi);
+		}
+	}
+
 	private ImageProcessor extractChannelFromInput(ChannelOverlay overlay) {
 		ImageStack stack = inputImp.getStack();
 		return stack.getProcessor(overlay.getChannel());
@@ -175,7 +180,7 @@ public class MontageCompiler implements Observer {
 	/**
 	 * Adapted from {@link ScaleBar}.
 	 */
-	private Roi generateScalebar(MontageItem item) {
+	private Point computeScalebarLocation(MontageItem item) {
 		// TODO Get from global settings
 		double barWidth = 4.5d;
 		
@@ -211,10 +216,79 @@ public class MontageCompiler implements Observer {
 			x = width/fraction;
 			y = height - height/fraction - barHeightInPixels - fontSize;
 		}
-
+		
 		int xShift = item.getColumn()*inputImp.getWidth() + item.getColumn()*paddingWidth;
 		int yShift = item.getRow()*inputImp.getHeight() + item.getRow()*paddingWidth;
 		
-		return new Roi(xShift + x, yShift + y, barWidthInPixels, barHeightInPixels);
+		return new Point(xShift + x, yShift + y);
+	}
+	
+	/**
+	 * Adapted from {@link ScaleBar}.
+	 */
+	private void addScalebarToOverlay(MontageItem item, Overlay overlay) {
+		// TODO Remove exisiting scalebar from overlay?
+		// overlay.remove(SCALE_BAR);
+		
+		// TODO Get from global settings
+		double barWidth = 4.5d;
+		
+		// TODO Get from global settings
+		String location = "Lower Right";
+		
+		// TODO Get from global settings
+		int barHeightInPixels = 2;
+		
+		// TODO Get from global settings
+		int fontSize = 42;
+		
+		// TODO Get from global settings
+		int paddingWidth = 10;
+		
+		// TODO Get from global settings
+		Color color = Color.WHITE;
+
+		Point scalebarLocation = computeScalebarLocation(item);
+		
+		int x = scalebarLocation.x;
+		int y = scalebarLocation.y;
+		int fontType = boldText?Font.BOLD:Font.PLAIN;
+		String face = serifFont?"Serif":"SanSerif";
+		Font font = new Font(face, fontType, fontSize);
+		String label = getLength(barWidth) + " "+ getUnits(inputImp);
+		ImageProcessor ip = inputImp.getProcessor();
+		ip.setFont(font);
+		int textWidth = hideText?0:ip.getStringWidth(label);
+		Calibration cal = inputImp.getCalibration();
+		int barWidthInPixels = (int)(barWidth/cal.pixelWidth);
+		Roi bar = new Roi(x, y, barWidthInPixels, barHeightInPixels);
+		bar.setFillColor(color);
+		bar.setStrokeColor(color);
+		overlay.add(bar);
+		
+		int xoffset = (barWidthInPixels - textWidth)/2;
+		int yoffset =  barHeightInPixels + (hideText?0:fontSize+fontSize/4);
+		if (!hideText) {
+			TextRoi text = new TextRoi(x+xoffset, y+yoffset, label, font);
+			text.setStrokeColor(color);
+			overlay.add(text);
+		}
+	}
+	
+	String getUnits(ImagePlus imp) {
+		String units = imp.getCalibration().getUnits();
+		if (units.equals("microns"))
+			units = IJ.micronSymbol+"m";
+		return units;
+	}
+	
+	String getLength(double barWidth) {
+		int digits = (int)barWidth==barWidth?0:1;
+		if (barWidth<1.0) digits=1;
+		if (digits==1) {
+			String s = IJ.d2s(barWidth/0.1, 2);
+			if (!s.endsWith(".00")) digits = 2;
+		}
+		return IJ.d2s(barWidth, digits);
 	}
 }
